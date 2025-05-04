7765a7110ca37c18527cc521f5081856b6565116
jasonjuntong <jjasonjuntong@gmail.com>
2025-05-04 11:46:48 +0800

    setup nuxt with supabase

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <UApp class="h-full">
+    <NuxtPage />
+  </UApp>
+</template>
+
---
+++ pages/login.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-screen h-full flex flex-col justify-center items-center">
+        <h1>Login</h1>
+    </div>
+</template>
